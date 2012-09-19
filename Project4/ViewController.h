a41d95e0266ecb9f3bca36e5c9b1d77cb5f3a860
Russell Carlin <recarlin@fullsail.com>
2012-09-18 20:50:00 -0400

    New project.

---
+++ Project4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project4
+//
+//  Created by Russell Carlin on 9/18/12.
+//  Copyright (c) 2012 Russell Carlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
